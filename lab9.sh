41fa1ed7c59e947f50a40e3ec1e261d264b6eb83
Caitlin <root@955331307.acs.edcc.edu>
2016-05-05 16:48:30 -0700

    made lab9.sh

---
+++ lab9.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+while getopts :dvhn:l: OPT ;do
+	case $OPT in
+		d) set -x;;
+		v) echo "VERBOSE variable is now set to true" 
+				vision=true;;
+		h) echo "LAB9.SH OPPTIONS:
+			-d 	turns on debugging
+			-v 	sets verbose to true
+			-h 	prints help message
+			-n 	takes name argument and prints message
+			-l 	lists directory using for loop (asks Y/N)";;
+		n)NAME=$OPTARG
+			echo "$NAME";;
+		l)DIRNAME=$OPTARG
+			if [[ -z $DIRNAME ]]
+				then
+				echo "Please enter a directory name"
+			fi
+			echo "Would you like to loop through $DIRNAME (Y/N?)"
+				read RESPONSE
+			if echo $RESPONSE | grep -qi "^y"
+			then
+				ls $DIRNAME
+			else
+				exit 1
+			fi;;
+		\?) echo "The last option you used was unknown. Use -h for help";;
+	esac
+done
+shift $(($OPTIND -1))
+
