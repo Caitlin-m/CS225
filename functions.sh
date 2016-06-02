e285199012182b54520cca96aeade8b668cc68eb
Caitlin <root@955331307.acs.edcc.edu>
2016-06-02 16:31:02 -0700

    updated functions.sh

--- functions.sh
+++ functions.sh
@@ -45,3 +45,47 @@ move()
 	mv $LINE "$HOME/$YEAR/$MONTH/$DAY"
 }
 
+#ip address
+check_ip()
+{
+        if [[ $INFO =~ ^(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9]) ]]
+        then
+                return 0
+        else
+                return 1
+        fi
+}
+
+#social security number
+check_ssn()
+{
+        if [[ $INFO =~ ^([0-9][0-9][0-9])-([0-9][0-9])-([0-9][0-9][0-9][0-9]) ]]
+        then
+                return 0
+        else
+                return 1
+        fi
+}
+
+#phone number
+check_pn()
+{
+        if [[ $INFO =~ ^([1-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]) ]]
+        then
+                return 0
+        else
+                return 1
+        fi
+}
+
+#credit card number
+check_ccn()
+{
+        if [[ $INFO =~ ^([0-9][0-9][0-9][0-9])([0-9][0-9][0-9][0-9])([0-9][0-9][0-9][0-9])([0-9][0-9][0-9][0-9]) ]]
+        then
+                return 0
+        else
+                return 1
+        fi
+}
+
