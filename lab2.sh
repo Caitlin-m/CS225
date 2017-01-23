e1fb48bf2fcec004014ac557372c3ee981939eaa
Caitlin McMurchie <c.mcmurchie1334@edmail.edcc.edu>
2017-01-23 15:57:27 -0800

    Update lab2.sh

--- lab2.sh
+++ lab2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#---------------------------------------------------------------------------------------------------
 #Prompts the user for a number, ensure it's a number and check to see if it's an even or odd number.
+#---------------------------------------------------------------------------------------------------
 
 echo "Enter a number, please"
 read NUMBER
@@ -10,7 +12,7 @@ CLEAN_NUMBER=$(echo $NUMBER | sed 's/^0*//' | sed 's/[.].*$//')
 #checks if it's a number
 if [ $CLEAN_NUMBER -eq $CLEAN_NUMBER 2>/dev/null ]
 then
-#if it is it checks if it's even or add
+#if it is it checks if it's even or odd
 	if [ "$(( CLEAN_NUMBER % 2 ))" -ne 0 ]
 	then
 
