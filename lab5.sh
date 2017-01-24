5899942b8b873ec4ad2f3376cb41392bfa160bab
Caitlin McMurchie <c.mcmurchie1334@edmail.edcc.edu>
2017-01-23 16:19:48 -0800

    Update lab5.sh

--- lab5.sh
+++ lab5.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#Loops through medialist.txt  and check whick files exist in medialab, if it does the it pipes it to foundfiles.txt, if it's lost then lostfiles.txt
-
+#==============================================================================
+#Loops through medialist.txt  and check whick files exist in medialab, 
+#if it does the it pipes it to foundfiles.txt, if it's lost then lostfiles.txt
+#==============================================================================
 
 rm -f /home/foundfiles.txt
 rm -f /home/lostfiles.txt
