b8de828da03f187c1b0b066da2dffedeb0b73ba7
Caitlin McMurchie <c.mcmurchie1334@edmail.edcc.edu>
2018-03-05 17:20:53 -0800

    Update lab1.sh
    Checks to see if a number is less than, equal or greater than 5

--- lab1.sh
+++ lab1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #----------------------------------------------------------------
-#Checks to see if a number is less than, equil or greater than 5
+#Checks to see if a number is less than, equal or greater than 5
 #----------------------------------------------------------------
 
 echo " Enter a number, please"
